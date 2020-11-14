7a9c8784e1840814d5061e436dcc45c2112f2507
yuejianjian <yuejianjian@kuaixiaohulian.com>
2020-11-14 18:03:38 +0800

    随便改改

--- src/utils/request.js
+++ src/utils/request.js
@@ -22,6 +22,7 @@ service.interceptors.request.use(function (config) {
 
 // 添加响应拦截器
 service.interceptors.response.use(function (response) {
+  
     // 对响应数据做点什么
     //return response;
     let data = response.data;
